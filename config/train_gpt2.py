2075d91ce56a6f486b1d175e3416881a09632712
JeevanBhoot <jeevan.bhoot@gmail.com>
2025-10-19 09:43:16 +0000

    Update training config

--- config/train_gpt2.py
+++ config/train_gpt2.py
@@ -3,18 +3,18 @@
 # $ torchrun --standalone --nproc_per_node=8 train.py config/train_gpt2.py
 
 wandb_log = True
-wandb_project = 'owt'
-wandb_run_name='gpt2-124M'
+wandb_project = 'exo'
+wandb_run_name='tp_spd_v2_4gpu_23bs_12gas'
 
 # these make the total batch size be ~0.5M
 # 12 batch size * 1024 block size * 5 gradaccum * 8 GPUs = 491,520
-batch_size = 12
+batch_size = 23
 block_size = 1024
-gradient_accumulation_steps = 5 * 8
+gradient_accumulation_steps = 3 * 4
 
 # this makes total number of tokens be 300B
-max_iters = 600000
-lr_decay_iters = 600000
+max_iters = 10000
+lr_decay_iters = 10000
 
 # eval stuff
 eval_interval = 1000
